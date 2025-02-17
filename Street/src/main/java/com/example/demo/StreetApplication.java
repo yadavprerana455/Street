c0bb756592b91ca9d0a81a9eb67f365516f10309
yadavprerana455 <yadavprerana455@gmail.com>
2025-02-17 22:22:14 +0530

    First commit

---
+++ Street/src/main/java/com/example/demo/StreetApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetApplication.class, args);
+	}
+
+}
